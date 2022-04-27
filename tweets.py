9bad27d2238929339aa9e1587ea955409396a34a
Sai Phani Sharath Chandra Danthalapelli <sharathchandra@pal-10-184-8-138.itap.purdue.edu>
2022-04-27 15:07:47 -0400

    models

--- tweets.py
+++ tweets.py
@@ -2,7 +2,7 @@ import snscrape.modules.twitter as sntwitter
 import pandas as pd
 
 # query = "(from:elonmusk) until:2020-01-01 since:2010-01-01"
-query = '"ukraine" and "russia"  ("war" OR or OR "operation" OR or OR "military" OR or OR "putin") min_faves:60 min_retweets:40 until:2022-03-30 since:2022-02-14'
+query = '"ukraine"  ("war" OR or OR "operation" OR or OR "military" OR or OR "putin") min_faves:60 min_retweets:40 until:2022-03-30 since:2022-02-14'
 tweets = []
 limit = 1000000
 
