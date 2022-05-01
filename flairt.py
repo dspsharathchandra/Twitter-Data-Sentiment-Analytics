7ec295fd15f4d4623ac550d433be19bdb646c0fd
Aditya Gupta <adityagupta1089@gmail.com>
2022-05-01 13:54:22 -0400

    Remove some files

--- flairt.py
+++
@@ -1,6 +0,0 @@
-import flair
-flair_sentiment = flair.models.TextClassifier.load('en-sentiment')
-s = flair.data.Sentence("This is good idea")
-flair_sentiment.predict(s)
-total_sentiment = s.labels
-print(total_sentiment)
